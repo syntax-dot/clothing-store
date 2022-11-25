3a525824813663cbdde69be7d3658289cde17216
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-11-25 19:52:36 +0300

    wip

---
+++ src/app/components/RatingStars/RatingStars.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="$style.root">
+    <div v-for="star in rating">
+      <img src="./icons/activeStar.svg">
+      <img src="./icons/inactiveStar.svg">
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { RatingStarsProps } from './RatingStars.props'
+
+defineProps<RatingStarsProps>()
+</script>
+
+<style module lang="scss">
+.root {
+  width: 100%;
+  height: 100%;
+}
+</style>
