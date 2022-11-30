2080fdd9a9795aec996bdb2088183bb2926e709b
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-11-30 21:30:22 +0300

    wip

--- src/app/components/ProductView/ImagesPath.ts
+++ src/app/components/ProductView/ImagesPath.ts
@@ -1,7 +1,12 @@
 import { groupBy, mapValues } from 'lodash'
-const images = import.meta.glob('../../../assets/products/**/*', { eager: true })
 
-const allImagesPaths: [string, string][] = Object.entries(images).map(v => [prepareKey(v[0]), v[1]?.default])
+const images: Record<string, { default: string }> = import.meta.glob('../../../assets/products/**/*', { eager: true })
+// const images = import.meta.glob('../../../assets/products/**/*', { eager: true })
+
+// const allImagesPaths: [string, string][] = Object.entries(images).map(v => [prepareKey(v[0]), v[1].default])
+const allImagesPaths: [string, string][] = Object.entries(images).map(
+  ([path, module]) => [prepareKey(path), module.default],
+)
 
 function prepareKey(key: string): string {
   return key.split('/').slice(-2).join('/')
